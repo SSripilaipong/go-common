23fc7cf74ac509963a707f3f538cc7ed007164b7
SSripilaipong <SHSnail@mail.com>
2025-07-21 11:25:25 +0700

    init: moved from muto project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SSripilaipong/go-common
+
+go 1.23.8
---
+++ rods/map.go
@@ -0,0 +1,24 @@
+package rods
+
+import (
+	"maps"
+
+	"github.com/SSripilaipong/go-common/optional"
+)
+
+type Map[K comparable, V any] struct {
+	data map[K]V
+}
+
+func NewMap[K comparable, V any](data map[K]V) Map[K, V] {
+	return Map[K, V]{data: data}
+}
+
+func (m Map[K, V]) Get(key K) optional.Of[V] {
+	v, ok := m.data[key]
+	return optional.New(v, ok)
+}
+
+func (m Map[K, V]) ToMap() map[K]V {
+	return maps.Clone(m.data)
+}
