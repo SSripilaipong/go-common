1d471e1eba5cc18f5ca7e31b252a06acf72889a7
SSripilaipong <SHSnail@mail.com>
2025-08-13 22:12:14 +0700

    refactor(optional): use pointer instead of bool flag

--- optional/optional.go
+++ optional/optional.go
@@ -1,12 +1,11 @@
 package optional
 
 type Of[T any] struct {
-	x       T
-	isEmpty bool
+	x *T
 }
 
 func Value[T any](x T) Of[T] {
-	return Of[T]{x: x, isEmpty: false}
+	return Of[T]{x: &x}
 }
 
 func ValueOf[T any](x Of[T]) T {
@@ -14,25 +13,34 @@ func ValueOf[T any](x Of[T]) T {
 }
 
 func Empty[T any]() Of[T] {
-	return Of[T]{isEmpty: true}
+	return Of[T]{x: nil}
 }
 
-func (o Of[T]) Value() T {
-	return o.x
+func (o Of[T]) Value() (x T) {
+	if o.x == nil {
+		return x
+	}
+	return *o.x
 }
 
 func (o Of[T]) IsEmpty() bool {
-	return o.isEmpty
+	return o.x == nil
 }
 
 func (o Of[T]) IsNotEmpty() bool {
 	return !o.IsEmpty()
 }
 
-func (o Of[T]) Return() (T, bool) {
-	return o.x, !o.isEmpty
+func (o Of[T]) Return() (x T, ok bool) {
+	if o.x == nil {
+		return x, false
+	}
+	return *o.x, true
 }
 
-func New[T any](node T, exists bool) Of[T] {
-	return Of[T]{x: node, isEmpty: !exists}
+func New[T any](x T, exists bool) Of[T] {
+	if exists {
+		return Value(x)
+	}
+	return Empty[T]()
 }
